d2d27e50662e1b0d0677a79c85a61725751505b7
Harishini <harishini2002@gmail.com>
2022-01-24 17:34:22 +0530

    commited

--- my_django_port/my_django_port/settings.py
+++ my_django_port/my_django_port/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$@fjg6^9xe8lbf3uxrq&&_m8ff42y5lbdph$+rh&8oq$^h))v2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['harishiniportfolio.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','harishiniportfolio.herokuapp.com']
 
 
 # Application definition
